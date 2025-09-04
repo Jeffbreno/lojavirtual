d862b7f30d572f5cc6c94edead888145d62c9e12
Jefferson Lopes <jeffbreno@gmail.com>
2025-09-04 12:40:42 -0300

    feat:Colocar configuração do email para testes

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -168,3 +168,11 @@ STATIC_ROOT = BASE_DIR / 'backend' / 'static'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'sua_loja@email.com'
